53f92d02d666d4c9f0213b40502a43df187f5600
justtucker997 <justin.tucker997@gmail.com>
2024-08-11 15:11:41 -0700

    js practice dir added and first 2 odin modules started

---
+++ javascript_prac/01/script.js
@@ -0,0 +1,14 @@
+if (browser == "Edge") {
+    alert('youve got the edge');
+}
+
+else if (browser == "Chrome"
+    || browser == "Firefox"
+    || browser == "Safari"
+    || browser == "Opera") {
+        alert('we support these browsers too ');
+    }
+
+else {
+    alert('we hope this page looks okay');
+}
